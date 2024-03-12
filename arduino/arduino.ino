19604d6d0b7187b7b22d27f08764a326fa4bafa5
Sim49 <161430685+Sim49@users.noreply.github.com>
2024-03-12 21:23:42 +0100

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -26,6 +26,8 @@ float capteurPression;
 float pressionSenKy052;
 float temperatureSenKy052;
 int continuer = 1;
+int data;
+int pos;
 
 Servo myservo;  // create servo object to control a servo
 
@@ -92,4 +94,5 @@ void servomoteur(){
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
