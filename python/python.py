9bd7c37315d59532013c61a81b15ace3d958e400
spiderLambo <spiderLambo.dev@outlook.com>
2024-03-26 20:30:04 +0100

    distanceSenKy052 et split

--- python/python.py
+++ python/python.py
@@ -22,18 +22,10 @@ TAILLE = (300, 0)  # Taille de la fenetre
     capteurPression,
     pressionSenKy052,
     temperatureSenKy052,
-) = ([], [], [], [], [], [], [])
+    distanceSenKy052,
+) = ([], [], [], [], [], [], [], [])
 compteur = 0
 moy_temperature, moy_pression = 20, 101
-
-
-# Fonction pour trouver les points virgules
-def point_virgules(chaine_de_caracteres):
-    indices_point_virgules = []
-    for i in range(len(donne)):
-        if ord(donne[i]) == 59:
-            indices_point_virgules.append(j)
-    return indices_point_virgules
 
 
 # Fonction pour faire un graphique pour chaque grandeur
@@ -48,7 +40,7 @@ def graph(temps, grandeur, lab, nom):
 
 # Entête du fichier CSV
 fichier.write(
-    "Temps (en s);Temperature (en *C);Valeur du potentiometre lineaire;Distance (en m);Pression (en kPa);Pression du capteur SEN KY052(en kPa);Temperature du capteur SEN KY052(en *C)\n"
+    "Temps (en s);Temperature (en *C);Valeur du potentiometre lineaire;Distance (en m);Pression (en kPa);Pression du capteur SEN KY052(en kPa);Temperature du capteur SEN KY052(en *C);Distance du capteur SEN KY052(en m)\n"
 )
 
 # Lancement de la fenetre
@@ -69,20 +61,21 @@ while run:
         donnees_arduino = portserie.readline().decode("ascii")
         compteur += 1
 
-        lpv = point_virgules(donnees_arduino)  # index des points virgules
+        fragment = donnees_arduino.split(";")  # index des points virgules
 
         # mise à jour des listes
         time.append(compteur)
-        thermistance.append(donnees_arduino[: lpv[0]])
-        potentiometreLineaire.append(donnees_arduino[lpv[0] + 1 : lpv[1]])
-        capteurDistance.append(donnees_arduino[lpv[1] + 1 : lpv[2]])
-        capteurPression.append(donnees_arduino[lpv[2] + 1 : lpv[3]])
-        pressionSenKy052.append(donnees_arduino[lpv[3] + 1 : lpv[4]])
-        temperatureSenKy052.append(donnees_arduino[lpv[4] + 1 : lpv[5]])
+        thermistance.append(fragment[0])
+        potentiometreLineaire.append(fragment[1])
+        capteurDistance.append(fragment[2])
+        capteurPression.append(fragment[3])
+        pressionSenKy052.append(fragment[4])
+        temperatureSenKy052.append(fragment[5])
+        distanceSenKy052.append(fragment[6])
 
         # écriture sur le fichier csv
         fichier.write(
-            f"{compteur};{thermistance[-1]};{potentiometreLineaire[-1]};{capteurDistance[-1]};{capteurPression[-1]};{pressionSenKy052[-1]};{temperatureSenKy052[-1]}\n"
+            f"{compteur};{thermistance[-1]};{potentiometreLineaire[-1]};{capteurDistance[-1]};{capteurPression[-1]};{pressionSenKy052[-1]};{temperatureSenKy052[-1]};{distanceSenKy052[-1]}\n"
         )
 
         # messages d'erreur
@@ -121,3 +114,4 @@ graph(time, capteurDistance, "Distance (m)", "Capteur de distance")
 graph(time, capteurPression, "Pression (kPa)", "Capteur de pression")
 graph(time, pressionSenKy052, "Pression (kPa)", "Capteur de pression SEN-KY052")
 graph(time, temperatureSenKy052, "Temperature (°C)", "Capteur de temperature SEN-KY052")
+graph(time, distanceSenKy052, "Distance (m)", "Capteur de distance SEN-KY052")
